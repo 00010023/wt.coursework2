cdfdd2ba0b176b5640475cbf893a2dddac42bf93
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-02 04:11:18 +0500

    fixed header + added back button

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="flex items-center justify-center w-full h-24 border-t">
     <div className="flex items-center justify-center">
-      Created by{" "}
+      Crafted by{" "}
       <a
         href="https://github.com/00010023"
         className="font-bold m-1 hover:text-blue-600"
--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,4 +1,18 @@
 import "../styles/globals.css";
+import "../styles/ngprogress.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+NProgress.configure({
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => {
+  window.scrollTo(0, 0);
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
