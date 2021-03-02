9d751eb3c2483412e7ed1bed970f2f452f4cf633
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-03 02:24:12 +0500

    Automized updates by GenoGit

---
+++ scripts/client.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd "$(dirname "$0")" || exit
+
+bootstrap() {
+  if ! command -v yarn &> /dev/null
+  then
+    npm run dev
+  else
+    yarn dev
+  fi
+}
+
+cd ../client || exit
+bootstrap
+exit 0
---
+++ scripts/server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd "$(dirname "$0")" || exit
+
+bootstrap() {
+  if ! command -v yarn &> /dev/null
+  then
+    npm run dev
+  else
+    yarn dev
+  fi
+}
+
+cd ../server || exit
+bootstrap
+exit 0
