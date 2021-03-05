0173be6011ce121768642f16400f25b2bcba8b55
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-06 04:54:13 +0500

    Automized updates by GenoGit

--- client/components/Notification.jsx
+++ client/components/Notification.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Link from "next/link";
 
 const Notification = ({ news }) => {
+  const file =
+    "https://intranet.wiut.uz/Uploads/CourseworkDescriptionFiles/fabbcb3f-c585-48db-a904-523c79adb7ab.pdf";
+
   const NewsPanel = (news) => {
     return (
       <div className="notification-area">
         <div className="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
           <div className="pr-16 sm:text-center sm:px-16">
             <p className="font-medium text-black">
-              <a
-                href="https://t.me/SeventyPlusBIS/230"
-                className="text-black cursor-help"
-              >
+              <a href={file} className="text-black cursor-help">
                 <span>
                   <span className="font-bold">Warning:</span>
                   {news}
