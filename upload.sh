f2b322f2c50b7b6b5d5bce238f9c5cd25a021746
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-07 23:15:46 +0500

    Automized updates by GenoGit

--- upload.sh
+++ upload.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
 cd ..
-zip -r 00010023WT ./wt.coursework2 -x *.git* */node_modules/**\* */.idea/**\* */.next/**\* *.DS_Store*
+zip -r 00010023WT ./wt.coursework2 -x \
+        ./wt.coursework2/.git/\* \
+        ./wt.coursework2/.idea/\* \
+        ./wt.coursework2/node_modules/\* \
+        ./wt.coursework2/docs/node_modules/\* \
+        ./wt.coursework2/server/node_modules/\* \
+        ./wt.coursework2/server/.env \
+        ./wt.coursework2/client/node_modules/\* \
+        ./wt.coursework2/client/.next/\* \
+        ./wt.coursework2/.next/\* \
+        ./wt.coursework2/.DS_Store
 cd ./wt.coursework2 || exit
 [ ! -d "./upload" ] && mkdir upload
 BACKUP=$(date +'%m-%d-%Y')
