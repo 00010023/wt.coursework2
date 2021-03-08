077620786a92ca2c6066e43e2afd26e17e92c283
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-08 20:20:47 +0500

    Automized updates by GenoGit

--- scripts/update-client.sh
+++ scripts/update-client.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
 cd ../
+cd ../webtech || exit
+rm -v !(".git")
+cd ../wt.coursework2 || exit
 cp -aRf ./client/. ../webtech/
 cd ../webtech || exit
 git add .
