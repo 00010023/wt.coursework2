c25f7c911199a69fd08eb839745bc82cac826b72
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-08 20:28:39 +0500

    Automized updates by GenoGit

--- scripts/update-client.sh
+++ scripts/update-client.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
+
+there-is-no-client() {
+  git clone https://github.com/genemators/webtech temporary
+  mv ./temporary/.git ./
+  rm -rf ./temporary
+}
+
+upload-client() {
+  git add .
+  git commit -m "Updating..."
+  git push
+}
+
 cd ../
 cd ../webtech || exit
 find . -type f -not -name '.git'-delete
 cd ../wt.coursework2 || exit
 cp -aRf ./client/. ../webtech/
 cd ../webtech || exit
-git add .
-git commit -m "Updating..."
-git push
+[ ! -d ".git" ] && there-is-no-client
+[ -d ".git" ] && update-client
 exit 0
